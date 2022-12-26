f2ca50625e0bdd7409f251add2f6fc7d4800c7cf
Gildo Junior <1gildojunior@gmail.com>
2022-12-26 19:40:30 -0300

    License and typescript configurations

--- build/index.js
+++ build/index.js
@@ -31,92 +31,51 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
-var __generator = (this && this.__generator) || function (thisArg, body) {
-    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
-    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
-    function verb(n) { return function (v) { return step([n, v]); }; }
-    function step(op) {
-        if (f) throw new TypeError("Generator is already executing.");
-        while (g && (g = 0, op[0] && (_ = 0)), _) try {
-            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
-            if (y = 0, t) op = [op[0] & 2, t.value];
-            switch (op[0]) {
-                case 0: case 1: t = op; break;
-                case 4: _.label++; return { value: op[1], done: false };
-                case 5: _.label++; y = op[1]; op = [0]; continue;
-                case 7: op = _.ops.pop(); _.trys.pop(); continue;
-                default:
-                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
-                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
-                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
-                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
-                    if (t[2]) _.ops.pop();
-                    _.trys.pop(); continue;
-            }
-            op = body.call(thisArg, _);
-        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
-        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
-    }
-};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var core = __importStar(require("@actions/core"));
-var github = __importStar(require("@actions/github"));
-var app_live_1 = require("./http-requests/app-live");
-var api_app_live_1 = __importDefault(require("./utils/api-app-live"));
+const core = __importStar(require("@actions/core"));
+const github = __importStar(require("@actions/github"));
+const app_live_1 = require("./http-requests/app-live");
+const api_app_live_1 = __importDefault(require("./utils/api-app-live"));
 (function () {
-    return __awaiter(this, void 0, void 0, function () {
-        var appPath, bsUserName, bsAccessKey, appToReplace_1, apps, app, payload, error_1;
-        return __generator(this, function (_a) {
-            switch (_a.label) {
-                case 0:
-                    _a.trys.push([0, 7, , 8]);
-                    appPath = core.getInput('app-path');
-                    if (!appPath)
-                        core.setFailed('app-path is required');
-                    bsUserName = core.getInput('browserstack-username');
-                    if (!bsUserName)
-                        core.setFailed('browserstack-username is required');
-                    bsAccessKey = core.getInput('browserstack-accesskey');
-                    if (!bsAccessKey)
-                        core.setFailed('browserstack-accesskey is required');
-                    if (!(appPath && bsUserName && bsAccessKey)) return [3 /*break*/, 6];
-                    api_app_live_1.default.defaults.auth = {
-                        username: bsUserName,
-                        password: bsAccessKey
-                    };
-                    appToReplace_1 = core.getInput("app-to-replace");
-                    console.log("appPath -  ".concat(appPath, "!"));
-                    if (!appToReplace_1) return [3 /*break*/, 4];
-                    return [4 /*yield*/, (0, app_live_1.getRecentApps)()];
-                case 1:
-                    apps = _a.sent();
-                    if (!(apps && apps.length > 0)) return [3 /*break*/, 4];
-                    app = apps.find(function (app) { return app.app_name === appToReplace_1; });
-                    if (!app) return [3 /*break*/, 3];
-                    return [4 /*yield*/, (0, app_live_1.removeApp)({ appId: app.app_id })];
-                case 2:
-                    _a.sent();
-                    return [3 /*break*/, 4];
-                case 3:
-                    console.log("appToReplace informado não encontrado para o usuário em questão!");
-                    _a.label = 4;
-                case 4: return [4 /*yield*/, (0, app_live_1.uploadApp)({ appPath: appPath })];
-                case 5:
-                    _a.sent();
-                    _a.label = 6;
-                case 6:
-                    payload = JSON.stringify(github.context.payload, undefined, 2);
-                    console.log("The event payload: ".concat(payload));
-                    return [3 /*break*/, 8];
-                case 7:
-                    error_1 = _a.sent();
-                    core.setFailed(error_1.message);
-                    return [3 /*break*/, 8];
-                case 8: return [2 /*return*/];
+    return __awaiter(this, void 0, void 0, function* () {
+        try {
+            const appPath = core.getInput('app-path');
+            if (!appPath)
+                core.setFailed('app-path is required');
+            const bsUserName = core.getInput('browserstack-username');
+            if (!bsUserName)
+                core.setFailed('browserstack-username is required');
+            const bsAccessKey = core.getInput('browserstack-accesskey');
+            if (!bsAccessKey)
+                core.setFailed('browserstack-accesskey is required');
+            if (appPath && bsUserName && bsAccessKey) {
+                api_app_live_1.default.defaults.auth = {
+                    username: bsUserName,
+                    password: bsAccessKey
+                };
+                const appToReplace = core.getInput("app-to-replace");
+                console.log(`appPath -  ${appPath}!`);
+                if (appToReplace) {
+                    const apps = yield (0, app_live_1.getRecentApps)();
+                    if (apps && apps.length > 0) {
+                        const app = apps.find(app => app.app_name === appToReplace);
+                        if (app)
+                            yield (0, app_live_1.removeApp)({ appId: app.app_id });
+                        else
+                            console.log("appToReplace informado não encontrado para o usuário em questão!");
+                    }
+                }
+                yield (0, app_live_1.uploadApp)({ appPath });
             }
-        });
+            // Get the JSON webhook payload for the event that triggered the workflow
+            const payload = JSON.stringify(github.context.payload, undefined, 2);
+            console.log(`The event payload: ${payload}`);
+        }
+        catch (error) {
+            core.setFailed(error.message);
+        }
     });
 })();
--- build/utils/api-app-live.js
+++ build/utils/api-app-live.js
@@ -3,7 +3,7 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-var axios_1 = __importDefault(require("axios"));
+const axios_1 = __importDefault(require("axios"));
 exports.default = axios_1.default.create({
     baseURL: "https://api-cloud.browserstack.com/app-live/",
 });
